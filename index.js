7accc3497e26e57263ca7692eaf45e232e59eb2b
caio correa <caio.correa.clear@gmail.com>
2022-05-04 15:25:12 -0300

    aula 35 G7 Trabalhando com json

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-alert('Ola');
+// localizando campos
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+// informacoes do usuario
+var user = {};
+
+// verificando campos
+fields.forEach(function(field, index){
+
+    if(field.name == "gender"){
+        
+        if(field.checked){
+            user[field.name] = field.value;
+        }
+        
+    }else{
+        user[field.name] = field.value;
+    }
+    
+});
